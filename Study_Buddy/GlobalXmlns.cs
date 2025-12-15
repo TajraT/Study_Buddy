1f31df769365984f3a6a957fe6595b9bbecd077a
Tajra Tašić <tajra.tasic.24@size.ba>
2025-12-15 15:31:42 +0100

    Add project files.

---
+++ Study_Buddy/GlobalXmlns.cs
@@ -0,0 +1,2 @@
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "Study_Buddy")]
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "Study_Buddy.Pages")]
---
+++ Study_Buddy/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace Study_Buddy
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
