77e6d6a0c96a3d2b021f2f7a3bc8bbc5c80281eb
DzenyTech <dzenana.sehic.24@size.ba>
2026-01-11 18:58:36 +0100

    Revert "Merge branch 'A'"
    This reverts commit 331e1bae25d6628850933f26068544cab3384956.

--- Study_Buddy/TimerPage.xaml.cs
+++ Study_Buddy/TimerPage.xaml.cs
@@ -1,94 +1,20 @@
-using System;
-using System.Threading;
-using System.Threading.Tasks;
 using Microsoft.Maui.Controls;
 namespace Study_Buddy;
 
 public partial class TimerPage : ContentPage
 {
-
-    private int _seconds;
-    private CancellationTokenSource? _cts;
-
     public TimerPage()
     {
         InitializeComponent();
-        SetDefaultTime();
-    }
 
-    private void SetDefaultTime()
+    }
+    private async void HomeImage_Tapped(object sender,EventArgs e)
     {
-        _seconds = 25 * 60;
-        UpdateLabel();
+       await Navigation.PopModalAsync();
     }
-
-    private void UpdateLabel()
-    {
-        TimerLabel.Text = TimeSpan
-            .FromSeconds(_seconds)
-            .ToString(@"mm\:ss");
-    }
-
-    private async void Start_Clicked(object sender, EventArgs e)
-    {
-        if (_cts != null)
-            return;
-
-        if (int.TryParse(MinutesEntry.Text, out int minutes) && minutes > 0)
-        {
-            _seconds = minutes * 60;
-            UpdateLabel();
-
-
-            ConfirmText.Text = $"Focus session set to {minutes} minutes";
-            ConfirmPopup.IsVisible = true;
-        }
-
-        _cts = new CancellationTokenSource();
-
-        try
-        {
-            while (_seconds > 0)
-            {
-                await Task.Delay(1000, _cts.Token);
-                _seconds--;
-                UpdateLabel();
-            }
-        }
-        catch (TaskCanceledException) { }
-        finally
-        {
-            _cts = null;
-        }
-    }
-
-    private void ClosePopup_Clicked(object sender, EventArgs e)
-    {
-        ConfirmPopup.IsVisible = false;
-    }
-
-    private void Pause_Clicked(object sender, EventArgs e)
-    {
-        _cts?.Cancel();
-        _cts = null;
-    }
-
-    private void Restart_Clicked(object sender, EventArgs e)
-    {
-        _cts?.Cancel();
-        SetDefaultTime();
-    }
-
     private async void EnterFocusMode_Clicked(object sender, EventArgs e)
     {
-        await Navigation.PushAsync(new FocusModePage(_seconds));
-
-    }
-
-    private async void HomeImage_Tapped(object sender, EventArgs e)
-    {
-        await Navigation.PopModalAsync();
+        await Navigation.PushAsync(new FocusModePage());
     }
 }
-
-
+    
