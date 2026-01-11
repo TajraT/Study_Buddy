0dae466408d10e59b30090abcced4e75c12575a0
DzenyTech <dzenana.sehic.24@size.ba>
2026-01-11 18:53:40 +0100

    Revert "Merge branch 'A'"
    This reverts commit 331e1bae25d6628850933f26068544cab3384956.

--- Study_Buddy/FocusModePage.xaml.cs
+++ Study_Buddy/FocusModePage.xaml.cs
@@ -1,52 +1,16 @@
-using System;
-using System.Threading;
 using System.Threading.Tasks;
-using Microsoft.Maui.Controls;
 
 namespace Study_Buddy;
 
 public partial class FocusModePage : ContentPage
 {
-    private int _seconds;
-    private CancellationTokenSource? _cts;
-
-    public FocusModePage(int seconds)
-    {
-        InitializeComponent();
-        _seconds = seconds;
-        UpdateLabel();
-        StartTimer();
-    }
-
-    private void UpdateLabel()
-    {
-        FocusTimerLabel.Text = TimeSpan
-            .FromSeconds(_seconds)
-            .ToString(@"mm\:ss");
-    }
-
-    private async void StartTimer()
-    {
-        _cts = new CancellationTokenSource();
-
-        try
-        {
-            while (_seconds > 0)
-            {
-                await Task.Delay(1000, _cts.Token);
-                _seconds--;
-                UpdateLabel();
-            }
-        }
-        catch (TaskCanceledException) { }
-       
-        ExitButton.IsEnabled = true;
-        ExitButton.Opacity = 1;
-
-    }
+	public FocusModePage()
+	{
+		InitializeComponent();
+	}
 
     private async void ExitFocusMode_Clicked(object sender, EventArgs e)
     {
-        await Navigation.PopAsync();
+		await Navigation.PopAsync();
     }
 }
